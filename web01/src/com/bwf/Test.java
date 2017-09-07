c40c27c13dd1358a01ac815fcffbfc54d859cbf6
10834 <10834@LY>
2017-09-07 10:07:24 +0800

    add Test.java

---
+++ web01/src/com/bwf/Test.java
@@ -0,0 +1,19 @@
+package com.bwf;
+
+public class Test 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String [] args)
+	{
+		 int a[] = new int[]{1,2,3};
+		   for(int i=0;i<=a.length;i++){
+		      System.out.println(a[i]);
+		   }
+
+	}
+}
+
+
